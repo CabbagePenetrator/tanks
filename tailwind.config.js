ef4594fc78b1dad15bf7df0ee77d29469a795dd2
Cabbage <cabbagepenetrator@gmail.com>
2023-12-07 18:46:32 +0000

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/**/*.blade.php",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
